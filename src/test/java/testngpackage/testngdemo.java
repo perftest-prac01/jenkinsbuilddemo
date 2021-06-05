3aef18d67cbfcc1b83d2295dc807211bed4f613f
perftest-prac01 <perftester2424@gmail.com>
2021-06-05 16:44:10 +0100

    testng commit

---
+++ src/test/java/testngpackage/testngdemo.java
@@ -0,0 +1,15 @@
+package testngpackage;
+
+import org.testng.annotations.Test;
+
+public class testngdemo {
+
+        @Test
+        public void myapp(){
+            System.out.println("Test case no 1");
+        }
+        @Test
+        public void myapp2(){
+            System.out.println("Test case no 2");
+        }
+}
