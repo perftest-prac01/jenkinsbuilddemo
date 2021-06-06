6f11bd74fc5eefd423731db85baa0b761e407818
perftest-prac01 <perftester2424@gmail.com>
2021-06-06 12:17:32 +0100

    fail test case added

--- src/test/java/Test_App.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-public class Test_App {
-
-    @Test
-    public void test_app(){
-        System.out.println("Test case 1");
-    }
-}
--- src/test/java/testngpackage/testngdemo.java
+++ src/test/java/testngpackage/testngdemo.java
@@ -1,15 +1,21 @@
 package testngpackage;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class testngdemo {
 
         @Test
-        public void myapp(){
+        public void app(){
             System.out.println("Test case no 1");
         }
         @Test
-        public void myapp2(){
+        public void app1(){
             System.out.println("Test case no 2");
+        }
+        @Test
+        public void app2(){
+            System.out.println("Test case no 3");
+            Assert.fail();
         }
 }
